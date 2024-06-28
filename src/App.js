65ba651ba027b6272c2529506995aad41e4b2807
Deekshith M <deekshithm321@gmail.com>
2024-06-29 00:00:25 +0530

    main page is done note responsive

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import Nav from './components/navbar/Nav';
 import About from './components/pages/About';
 import Home from './components/pages/Home';
 import Contact from './components/pages/Contact';
+import Main from './components/pages/Main';
 
 function App() {
   return (
     <div className="app">
       <Nav/>
       <Routes>
-        <Route path='/About' element={<About/>}/>
+        <Route path='/' element={<Main/>} />
         <Route path='/Home' element={<Home/>}/>
+        <Route path='/About' element={<About/>}/>
         <Route path='/Contact' element={<Contact/>}/>
       </Routes>
     </div>
---
+++ src/components/pages/Main.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import '../Pages_css/Main.css'
+import { FaInstagram, FaLinkedin, FaFacebook} from 'react-icons/fa'
+import img1 from '../assects/WhatsApp Image 2024-06-28 at 23.04.45_78283539.jpg'
+import img2 from '../assects/WhatsApp Image 2024-06-28 at 23.04.46_46340f42.jpg'
+
+function Main() {
+  return (
+    <div className='main'>
+        <div className="text">
+            <h1 className="head">
+                Hi! i am Deekshith
+            </h1>
+            <h3>
+                I am a <span>Web Developer</span> from India
+            </h3>
+            <h5>
+                Welcome to my page...
+            </h5>
+        </div>
+            <div className='social'>
+                <a className='fb' href="https://www.facebook.com/profile.php?id=100072631754805"><FaFacebook/></a>
+                <a className='insta' href="https://www.instagram.com/dee_kshith_8_._/"><FaInstagram/></a>
+                <a className='lin' href="https://www.linkedin.com/in/deekshith-m-378bb9302/"><FaLinkedin/></a>
+            </div>
+            <div className="img">
+                <img className='img-side1' src={img1} alt="img1" />
+            </div>
+            <div className="img1">
+                <img className='img-side2' src={img2} alt="img2" />
+            </div>
+    </div>
+    )
+}
+
+export default Main
